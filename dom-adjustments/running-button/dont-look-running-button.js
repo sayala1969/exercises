220ce27f29b733120ee314b7842914ee2f9a6efe
apple <apple@MacBook-Air.local>
2026-02-15 16:43:49 -0600

    finished running button exercise

--- dom-adjustments/running-button/dont-look-running-button.js
+++ dom-adjustments/running-button/dont-look-running-button.js
@@ -1,8 +1,24 @@
-console.log('Running button exercise loaded. Please check the instructions in the HTML file.');
-document.querySelector('.running-button').addEventListener('hover', (event) => {
+
+function getRandomPositionEvent(event) {
     const randomX = Math.floor(Math.random() * window.innerWidth);
     const randomY = Math.floor(Math.random() * window.innerHeight);
     event.target.style.position = 'absolute';
     event.target.style.left = `${randomX}px`;
     event.target.style.top = `${randomY}px`;
-});
+}
+
+function runnerButtonClickEvent(event) {
+    // do something
+    document.querySelector('.scrim').style.display = 'flex';
+}
+
+document.querySelector('.pretty-button.run').addEventListener('mouseenter', getRandomPositionEvent);
+
+document.querySelector('.pretty-button.run').addEventListener('click', runnerButtonClickEvent);
+
+function freezeRunningButton() {
+    console.log('Freezing the running button!');
+    document.querySelector('.pretty-button.run').removeEventListener('mouseenter', getRandomPositionEvent);
+}
+
+document.querySelector('.pretty-button.freeze').addEventListener('click', freezeRunningButton);
